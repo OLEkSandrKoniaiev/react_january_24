f6cbda7e31af2401ec470f27acce2f9690884494
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2024-06-15 22:55:19 +0300

    homework10 true final

--- src/components/Pagination/PaginationComponent.tsx
+++ src/components/Pagination/PaginationComponent.tsx
@@ -1,56 +1,41 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC} from 'react';
 import {useSearchParams} from "react-router-dom";
-import {ICarPaginatedModel} from "../../models/ICarPaginatedModel";
-import {carService} from "../../services/carService";
-import CarsComponent from "../Cars/CarsComponent";
+import {IPaginatedPageModel} from "../../models/IPaginatedPageModel";
 
+interface IProps {
+    next: IPaginatedPageModel | null,
+    prev: IPaginatedPageModel | null
+}
 
-const PaginationComponent: FC = () => {
+const PaginationComponent: FC<IProps> = ({next, prev}) => {
 
     const [query, setQuery] = useSearchParams({
         page: '1'
     });
 
-    const [carsPaginatedObject, setCarsPaginatedObject] = useState<ICarPaginatedModel>({
-        items: [],
-        prev: null,
-        next: null,
-        total_items: 0,
-        total_pages: 0
-    });
-
-    useEffect(() => {
-        carService.getUserCars(query.get('page') || '1').then(value => {
-            if (value) {
-                setCarsPaginatedObject(value);
-            }
-        });
-    }, [query]);
 
     const changePage = (action: string) => {
         switch (action) {
             case 'prev':
-                setQuery({...carsPaginatedObject.prev});
+                setQuery({...prev});
                 break;
             case 'next':
-                setQuery({...carsPaginatedObject.next});
+                setQuery({...next});
                 break;
         }
     }
 
     return (
         <>
-            <CarsComponent cars={carsPaginatedObject.items}/>
-
             <div>
                 <button onClick={() => {
                     changePage('prev');
-                }} disabled={!carsPaginatedObject.prev}>
+                }} disabled={!prev}>
                     prev
                 </button>
                 <button onClick={() => {
                     changePage('next');
-                }} disabled={!carsPaginatedObject.next}>
+                }} disabled={!next}>
                     next
                 </button>
             </div>
--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "../styles/General.module.css"
 import PaginationComponent from "../components/Pagination/PaginationComponent";
 import classNames from "classnames";
+import {ICarPaginatedModel} from "../models/ICarPaginatedModel";
+import {useSearchParams} from "react-router-dom";
+import {carService} from "../services/carService";
+import CarsComponent from "../components/Cars/CarsComponent";
 
 
 const CarsPage = () => {
+
+    const [query, setQuery] = useSearchParams({
+        page: '1'
+    });
+
+    const [carsPaginatedObject, setCarsPaginatedObject] = useState<ICarPaginatedModel>({
+        items: [],
+        prev: null,
+        next: null,
+        total_items: 0,
+        total_pages: 0
+    });
+
+    useEffect(() => {
+        carService.getUserCars(query.get('page') || '1').then(value => {
+            if (value) {
+                setCarsPaginatedObject(value);
+            }
+        });
+    }, [query]);
+
     return (
         <div className={classNames(styles.justifyContent, styles.columnAlignCenter)}>
-            <PaginationComponent/>
+            <CarsComponent cars={carsPaginatedObject.items}/>
+            <PaginationComponent next={carsPaginatedObject.next} prev={carsPaginatedObject.prev}/>
         </div>
     );
 };
