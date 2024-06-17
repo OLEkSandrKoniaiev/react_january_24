b319f09ba29ea04217e5bcc2b499dbd9155c379a
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2024-06-17 15:29:55 +0300

    homework13 base

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./redux/store";
+import {decrement, increment} from "./slices/slice1";
 
 
 const App = () => {
-  return (
-      <div>
 
-      </div>
-  );
+    const value = useAppSelector((state) => state.slice1.value);
+
+    const dispatch = useAppDispatch();
+
+    return (
+        <div>
+            <h2>{value}</h2>
+            <button onClick={() => {
+                dispatch(increment(100));
+            }}>
+                increment
+            </button>
+            <button onClick={() => {
+                dispatch(decrement());
+            }}>
+                decrement
+            </button>
+        </div>
+    );
 };
 
 export default App;
